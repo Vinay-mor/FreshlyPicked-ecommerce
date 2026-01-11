545db4cfbe10a253c5118c69a3287e0c117a8c93
Vinay-mor <vinaymor2005@gmail.com>
2026-01-11 13:38:27 +0530

    api filters sorting

--- src/modules/products/ui/components/product-list.tsx
+++ src/modules/products/ui/components/product-list.tsx
@@ -1,19 +1,22 @@
 "use client"
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useProductFilters } from "../../hooks/use-product-filters";
 
-interface Props{
-    category?:string;
+interface Props {
+    category?: string;
 };
 
-export const ProductList=({category}:Props)=>{
-    const trpc=useTRPC();
-    const {data}=useSuspenseQuery(trpc.products.getMany.queryOptions({
+export const ProductList = ({ category }: Props) => {
+    const [filters] = useProductFilters();
+    const trpc = useTRPC();
+    const { data } = useSuspenseQuery(trpc.products.getMany.queryOptions({
         category,
+        ...filters,
     }));
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
-            {data?.docs.map((product)=>(
+            {data?.docs.map((product) => (
                 <div key={product.id} className="border rounded-md bg-white p-4">
                     <h2 className="text-xl font-medium">{product.name}</h2>
                     <p>${product.price}</p>
@@ -22,8 +25,8 @@ export const ProductList=({category}:Props)=>{
         </div>
     )
 }
-export const ProductListSkeleton=()=>{
-    return(
+export const ProductListSkeleton = () => {
+    return (
         <div>
             Loading...
         </div>
