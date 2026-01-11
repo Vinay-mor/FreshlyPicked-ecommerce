09056d3025661e9d84b3d62f0bd07c45895e4185
Vinay-mor <vinaymor2005@gmail.com>
2026-01-11 13:48:31 +0530

    typo fix

--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -23,7 +23,7 @@ const Page = async ({ params, searchParams }: Props) => {
     return (
         <HydrationBoundary state={dehydrate(queryClient)}>
             <div className="px-4 lg:px-12 py-8 flex flex-col gap-4">
-                <div className="flex flex-col lg:flex-row lg:items-cener gap-y-2 lg:gap-y-0 justify-between">
+                <div className="flex flex-col lg:flex-row lg:items-center gap-y-2 lg:gap-y-0 justify-between">
                     <p className="text-2xl font-medium">Curated for you</p>
                     <ProductSort/>
                 </div>
